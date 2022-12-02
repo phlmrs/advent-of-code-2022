69445a2f0ec0677a9e67db8b90675558bf456f7a
Philipp Lammers <p.lammers@basecom.de>
2022-12-02 18:57:11 +0100

    Refactor solution for day2

--- puzzles/week1/day2.py
+++ puzzles/week1/day2.py
@@ -1,54 +1,42 @@
 def p1(inp):
-    return 0
+    # (what the opponent plays, what you should play): the score
+    p1_dict = {
+        ('A', 'X'): 4,
+        ('B', 'X'): 1,
+        ('C', 'X'): 7,
+        ('A', 'Y'): 8,
+        ('B', 'Y'): 5,
+        ('C', 'Y'): 2,
+        ('A', 'Z'): 3,
+        ('B', 'Z'): 9,
+        ('C', 'Z'): 6,
+    }
+
+    return sum([p1_dict[(opponent, result)] for opponent, result in inp])
 
 
 def p2(inp):
-    return 0
+    # (what the opponent plays, how the round ends): the score
+    p2_dict = {
+        ('A', 'X'): 3,
+        ('B', 'X'): 1,
+        ('C', 'X'): 2,
+        ('A', 'Y'): 4,
+        ('B', 'Y'): 5,
+        ('C', 'Y'): 6,
+        ('A', 'Z'): 8,
+        ('B', 'Z'): 9,
+        ('C', 'Z'): 7,
+    }
 
+    return sum([p2_dict[(opponent, result)] for opponent, result in inp])
 
-points = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3
-}
-
-p1_dict = {
-    ('X', 'A'): 3,
-    ('X', 'B'): 0,
-    ('X', 'C'): 6,
-    ('Y', 'A'): 6,
-    ('Y', 'B'): 3,
-    ('Y', 'C'): 0,
-    ('Z', 'A'): 0,
-    ('Z', 'B'): 6,
-    ('Z', 'C'): 3,
-}
-
-p2_dict = {
-    ('A', 'X'): 3, #lose
-    ('B', 'X'): 1,
-    ('C', 'X'): 2,
-    ('A', 'Y'): 4, #draw
-    ('B', 'Y'): 5,
-    ('C', 'Y'): 6,
-    ('A', 'Z'): 8, #win
-    ('B', 'Z'): 9,
-    ('C', 'Z'): 7,
-}
 
 with open('../../input/week1/day2.txt') as file:
-    #sum = 0
-    #for line in file:
-    #    (opponent, you) = line.strip().split(' ')
-    #    sum += points[you] + p1_dict[(you, opponent)]
-    #print(sum)
-
-    sum = 0
+    inp = []
     for line in file:
-        (opponent, you) = line.strip().split(' ')
-        print(you, opponent)
-        sum += p2_dict[(opponent, you)]
-    print(sum)
+        (first, second) = line.strip().split(' ')
+        inp.append((first, second))
 
-    print('part 1: {}'.format(p1([])))
-    print('part 2: {}'.format(p2([])))
+    print('part 1: {}'.format(p1(inp)))
+    print('part 2: {}'.format(p2(inp)))
