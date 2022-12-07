87904263e852de4076ece564a9090df2932f1194
Philipp Lammers <p.lammers@basecom.de>
2022-12-07 12:09:03 +0100

    Add and refactor partial solution for day7

--- puzzles/week1/day7.py
+++ puzzles/week1/day7.py
@@ -12,33 +12,37 @@ def get_subfolder_size(sub_folder):
         if isinstance(item, int):
             folder_size += item
         else:
-            print(sub_folder, filesystem[sub_folder], item)
             folder_size += get_subfolder_size(item)
     filesystem[sub_folder] = [folder_size]
     return folder_size
 
 
 with open('../../input/week1/day7.txt') as file:
-    # TODO directories können den gleichen Namen haben
-    filesystem = {}
+    filesystem = {'/': []}
     current_dir = ''
     for line in file.readlines():
         cmd = line.strip().split(' ')
         if cmd[1] == 'cd':
             if cmd[2] == '..':
+                history = current_dir.split('_')
+                current_dir = ''
+                for item in history[:-1]:
+                    current_dir = current_dir + item + '_'
+                current_dir = current_dir.rstrip('_')
                 continue
-            if cmd[2] not in filesystem:
-                filesystem[cmd[2]] = []
-            current_dir = cmd[2]
+            if current_dir == '':
+                current_dir = '/'
+                continue
+            elif current_dir + '_' + cmd[2] not in filesystem:
+                filesystem[current_dir + '_' + cmd[2]] = []
+            current_dir = current_dir + '_' + cmd[2]
         elif cmd[1] == 'ls':
             continue
         elif cmd[0] == 'dir':
-            if cmd[1] not in filesystem[current_dir] and cmd[1] != current_dir:
-                filesystem[current_dir].append(cmd[1])
+            if (current_dir + '_' + cmd[1]) not in filesystem[current_dir] and (current_dir + '_' + cmd[1]) != current_dir:
+                filesystem[current_dir].append(current_dir + '_' + cmd[1])
         elif cmd[0].isalnum():
             filesystem[current_dir].append(int(cmd[0]))
-
-    print(filesystem)
 
     for folder in filesystem:
         folder_size = 0
@@ -46,7 +50,6 @@ with open('../../input/week1/day7.txt') as file:
             if isinstance(item, int):
                 folder_size += item
             else:
-                print(folder, filesystem[folder], item)
                 folder_size += get_subfolder_size(item)
         filesystem[folder] = [folder_size]
 
@@ -54,7 +57,7 @@ with open('../../input/week1/day7.txt') as file:
     for folder_size in filesystem.values():
         if folder_size[0] <= 100000:
             res += folder_size[0]
-    print(filesystem)
+
     print(res)
 
     print('part 1: {}'.format(p1([])))
